d6760581195f2ded6408dadf40faf34ba6a3f34e
Matheus Fernandes Fumero <93229553+MatheusFern086@users.noreply.github.com>
2025-12-08 21:19:01 -0300

    Adicionados Annotations para tamanho das colunas e inserts padrão das migrações
    Adicionados Annotations para tamanho das colunas e inserts padrão das migrações

---
+++ APICatalogo/APICatalogo/Migrations/20251209000730_PopulaProdutos.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                "VALUES ('Coca-Cola Zero', 'Refrigerante sabor cola 350ml', 4.99, 'coca_cola.jpg', 50, GETDATE(), 1)");
+
+            mb.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                "VALUES ('Lanche de Atum', 'Lanche de Atum com maionese', 8.50, 'atum.jpg', 10, GETDATE(), 2)");
+
+            mb.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+               "VALUES ('Pudim 100g', 'Pudim de leite condesado 100g', 6.75, 'pudim.jpg', 20, GETDATE(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Produtos");
+        }
+    }
+}
--- APICatalogo/APICatalogo/Models/Categoria.cs
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,8 +1,18 @@
-﻿namespace APICatalogo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Models;
+[Tags("Categorias")]
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
+
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
     //relacionamento 1:N (uma categoria para muitos produtos)
