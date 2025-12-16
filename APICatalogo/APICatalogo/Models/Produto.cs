a942a9a7c992b74f350fde1dab5b6d77a4f889d7
Matheus Fernandes Fumero <93229553+MatheusFern086@users.noreply.github.com>
2025-12-15 21:46:08 -0300

    Adicionados os CRUDS de Categorias
    Adicionados os CRUDS de Categorias

--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace APICatalogo.Models;
 [Table("Produtos")]
@@ -29,5 +30,6 @@ public class Produto
     public int CategoriaId { get; set; }
 
     //relacionamento N:1 (muitos produtos para uma categoria)
+    [JsonIgnore]
     public Categoria? Categoria { get; set; }
 }
